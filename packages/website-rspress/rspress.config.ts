0e2db739f69efb071151328827e6f7b21de46a9b
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-12-21 20:23:32 +0100

    try rspress

---
+++ packages/website-rspress/rspress.config.ts
@@ -0,0 +1,32 @@
+import * as path from 'node:path'
+import { defineConfig } from 'rspress/config'
+import { pluginPlayground } from '@rspress/plugin-playground'
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  outDir: 'build',
+
+  markdown: {
+    checkDeadLinks: true,
+    defaultWrapCode: true,
+  },
+
+  title: 'React Babylonjs',
+  // icon: '/rspress-icon.png',
+
+  plugins: [pluginPlayground({ defaultRenderMode: 'pure' })],
+
+  route: {
+    cleanUrls: true,
+  },
+
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/brianzinn/react-babylonjs',
+      },
+    ],
+  },
+})
